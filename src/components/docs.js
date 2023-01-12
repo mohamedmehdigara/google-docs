dc759a2be4d22051f462f90fd51f28e8469eb060
mohamedmehdigara <garamehdi57@yahoo.com>
2023-01-12 09:04:50 +0100

    added files

---
+++ src/components/docs.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Modal from './modals';
+
+export default function Docs() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    return (
+        <div className='docs-main'>
+            <h1>Docs Clone</h1>
+
+            <button
+                className='add-docs'
+                onClick={handleOpen}
+            >
+                Add a Document
+            </button>
+
+            <Modal
+                open={open}
+                setOpen={setOpen}
+            />
+        </div>
+    )
+}
